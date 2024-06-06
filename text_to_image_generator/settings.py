501e8640428b45e429f76ce99219d5c26d41551e
Sarthak <sarthakrangari788@gmail.com>
2024-06-06 15:26:42 +0530

    vercel config done

--- text_to_image_generator/settings.py
+++ text_to_image_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*$%#qmmbvub@svauorp9^x$4!2*qk1yhpg4$_=6*vps#ze(vs8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 
 # Application definition
