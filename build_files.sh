3f0e6412de6efe59ec95d8d52bddd13a8cf1018d
Sarthak <sarthakrangari788@gmail.com>
2024-06-06 16:26:59 +0530

    g

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.9 get-pip.py
-
+apt-get update && apt-get install -y python3-pip
 
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
